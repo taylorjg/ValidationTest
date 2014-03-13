932517ff1b25978463191d2f8185db3cc054bfe0
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-03-13 13:56:45 +0000

    Added code re saving sections and doing dirty checking. Also, added code to manipulate the settings.ignore selector.

--- script/ValidationTest.js
+++ script/ValidationTest.js
@@ -9,22 +9,37 @@
     $(document).ready(function() {
 
         var $outputPanel = $("#outputPanel");
+        var _validator;
 
         var _logMessage = function(message) {
             var existingText = $.trim($outputPanel.html());
             var separator = (existingText) ? "<br />" : "";
             var newText = existingText + separator + message;
-            $outputPanel.html(newText);
-            $outputPanel.show();
+            $outputPanel.html(newText).show();
         };
 
-        $("form").validate({
+        var _reset = function(clearFieldsToo) {
+            _validator.resetForm();
+            $outputPanel.empty().hide();
+            if (clearFieldsToo) {
+                $(":input").val("");
+            }
+        }
+
+        $("#resetBtn").click(function() {
+            _reset(false /* clearFieldsToo */);
+        });
+
+        $("#clearBtn").click(function() {
+            _reset(true /* clearFieldsToo */);
+        });
+
+        _validator = $("form").validate({
                 errorClass: "myerror",
                 errorPlacement: function(error, element) {
                     error.addClass("alert");
                     error.addClass("alert-warning");
                     error.appendTo(element.closest("td").next("td"));
-                    _logMessage(error.html());
                 },
                 rules: {
                 // contactDetailsSection
@@ -63,6 +78,81 @@
             }
         });
 
+//        var _validateContactSection = function() {
+//            var oldIgnoreSelector = _validator.settings.ignore;
+//            _validator.settings.ignore = "#deliveryAddressSection :input";
+//            var result = _validator.form();
+//            _validator.settings.ignore = oldIgnoreSelector;
+//            return result;
+//        };
+//
+//        var _validateDeliveryAddressSection = function() {
+//            var oldIgnoreSelector = _validator.settings.ignore;
+//            _validator.settings.ignore = "#contactDetailsSection :input";
+//            var result = _validator.form();
+//            _validator.settings.ignore = oldIgnoreSelector;
+//            return result;
+//        };
+
+        var _saveSection = function(sectionName) {
+            _logMessage("saving section " + sectionName);
+        };
+
+        var _validateAndSaveSectionIfValid = function(sectionName) {
+            var isValid = false;
+//            switch (sectionName) {
+//                case "contact":
+//                    isValid = _validateContactSection();
+//                    break;
+//                case "deliveryAddress":
+//                    isValid = _validateDeliveryAddressSection();
+//                    break;
+//                default:
+//                    _logMessage("Unknown section, dude!");
+//                    break;
+//            }
+
+            _ignorePristineSections();
+            isValid = _validator.form();
+
+            if (isValid) {
+                _saveSection(sectionName);
+            }
+        };
+
+        var _validateDirtySectionsExceptThisOne = function($sectionToIgnore) {
+            var $dirtySections = $("div[data-validation-is-dirty='true']").not($sectionToIgnore);
+            $dirtySections.each(function () {
+                var sectionName = $(this).attr("data-validation-model");
+                _validateAndSaveSectionIfValid(sectionName);
+            });
+        };
+
+        var _ignorePristineSections = function() {
+            var $dirtySections = $("div[data-validation-model]:not([data-validation-is-dirty])");
+            var bits = [];
+            bits.push(":hidden");
+            $dirtySections.each(function() {
+                var $dirtySection = $(this);
+                var bit = "#" + $dirtySection.attr("id") + " :input";
+                bits.push(bit);
+            });
+            var ignoreSelector = bits.join(", ");
+            _validator.settings.ignore = ignoreSelector;
+            _logMessage("_validator.settings.ignore: " + _validator.settings.ignore);
+        };
+
+        $(document).on("change keyup paste", "div[data-validation-model] :input", function () {
+            var $thisSection = $(this).closest("div[data-validation-model]");
+            $thisSection.attr("data-validation-is-dirty", "true");
+        });
+
+        $(document).on("focus", "div[data-validation-model] :input", function () {
+            var $thisSection = $(this).closest("div[data-validation-model]");
+            _validateDirtySectionsExceptThisOne($thisSection);
+        });
+
         $outputPanel.hide();
+        _ignorePristineSections();
     });
 }());
